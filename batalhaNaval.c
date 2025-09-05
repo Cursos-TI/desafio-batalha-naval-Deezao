a446a848c2ce48e3c5518484aaa284952252bcb6
Deezao <andrebesson19@gmail.com>
2025-09-05 15:20:15 +0000

    começo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,10 +5,66 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+
+    char letras[9] =  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J'};
+    int lateral, valorLetras = 0; 
+    int tabuleiro[10][10] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+   
+    
+        for (int lateral = 0; lateral < 10; lateral++){
+        
+        if (lateral > valorLetras)
+        {
+            (valorLetras < lateral);
+            printf("%d\n", lateral);
+            printf("%d %d %d %d %d %d %d %d %d", tabuleiro[0][0], tabuleiro[0][1], tabuleiro[0][2], tabuleiro[0][3], tabuleiro[0][4], tabuleiro[0][5], tabuleiro[0][6], tabuleiro[0][7], tabuleiro[0][8], tabuleiro[0][9]);
+        }else {
+            printf("   %c %c %c %c %c %c %c %c %c\n", letras[0],letras[1], letras[2], letras[3], letras[4], letras[5], letras[6], letras[7], letras[8]);  
+        }
+        }
+            
+        
+        
+    
+    
+    
+    
+    printf("\n");
+
+
+
+    int tabuleiro[10][10] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
+
+    //printf(" %d ", &tabuleiro);
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    
+
+    
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
